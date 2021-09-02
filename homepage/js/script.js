50dc90e31a97aa54bbf6b85e46f48c9cfa6c6314
Robert Osial <rob.osial@gmail.com>
2021-09-02 07:29:08 +0100

    initial commit

---
+++ homepage/js/script.js
@@ -0,0 +1,44 @@
+{
+    const calculateResult = (amount, currency) => {
+        const rateUSD = 3.8915;
+        const rateEUR = 4.5858;
+        const rateGBP = 5.3582;
+
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
+            case "GBP":
+                return amount / rateGBP;
+            case "USD":
+                return amount / rateUSD;
+        };
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong> ${result.toFixed(2)} ${currency}</strong>`;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
